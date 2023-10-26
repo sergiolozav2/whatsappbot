258d21fed97ff1cf85f907454f48f1b264dd5fb8
sergiolozav <sergiolozav@gmail.com>
2023-10-26 10:06:25 -0400

    Variable innecesaria

--- src/constants/env.js
+++ src/constants/env.js
@@ -3,7 +3,4 @@ import dotenv from "dotenv"
 dotenv.config()
 
 
-export const account_sid = process.env.ACCOUNT_SID
-export const token = process.env.AUTH_TOKEN
-
 export const chatgpt_key = process.env.CHATGPT_KEY
