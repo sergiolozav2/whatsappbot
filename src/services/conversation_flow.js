6aa434f7311a7e01f83cd2c705c6725d61e1742c
asistentesistemas2022 <asistemas@empacar.com.bo>
2023-10-26 14:16:13 -0400

    Prueba conexion

--- src/services/conversation_flow.js
+++ src/services/conversation_flow.js
@@ -1,18 +1,41 @@
-
-
-
 export const steps = {
-    inicio: {
-        message: "Bienvenido a CaimanTec",
-        nextStep: [steps.informacion,steps.comprar,steps.soporte]
-    },
-    informacion: {
-        message: "Seleccionaste información"
-    },
-    comprar: {
+  inicio: {
+    message: "Bienvenido a CaimanTec",
+    nextStep: [steps.informacion, steps.comprar, steps.soporte],
+  },
 
-    },
-    soporte: {
-        
-    }
-}
+  informacion: {
+    message: "Seleccionaste información",
+    nextStep: [steps.respuestaInformacion],
+  },
+  comprar: {
+    message: "Muestra productos",
+    nextStep: [steps.cantidad],
+  },
+  soporte: {
+    message: "Ingrese sus consultas",
+    nextStep: [steps.respuesta],
+  },
+
+  cantidad: {
+    message: "Pago",
+    nextStep: [steps.pago],
+  },
+  pago: {
+    message: "Lee QR",
+    nextStep: [steps.verificacion],
+  },
+  verificacion: {
+    message: "Pago recibido",
+    nextStep: [steps.message], // Agregar registro de cliente con crm?
+  },
+
+  respuesta: {
+    message: "Respuesta de ChatGPT",
+    nextStep: [steps.soporte, steps.inicio],
+  },
+  respuestaInformacion: {
+    message: "Respuesta de ChatGPT",
+    nextStep: [steps.informacion, steps.inicio],
+  },
+};
