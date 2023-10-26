33146b0303db221b9355ec3159dc49de32e2ccc6
sergiolozav <sergiolozav@gmail.com>
2023-10-26 18:52:11 -0400

    Bot responde predefinido

--- src/app.js
+++ src/app.js
@@ -22,49 +22,53 @@ server.get("/", async (request, reply) => {
 });
 
 server.post("/webhook", async (request, reply) => {
-  const message = objectFromWebhookResponse(request.body);
-  console.log(message);
-  if (message.type !== MessageTypes.text) {
-    return { sucess: "false" };
+  try {
+    const message = objectFromWebhookResponse(request.body);
+
+    if (message.type !== MessageTypes.notify) {
+      return { success: "false" };
+    }
+    const user = await getUserByPhone(message.from);
+
+    if (!user) {
+      return { success: "false" };
+      return;
+    }
+
+    let nextStep = "";
+    const step = user.conversation_step;
+    console.log(user);
+    switch (step) {
+      case "nuevo":
+        await sendWhatsappText(
+          user.phone,
+          "Bienvenido a CaimanTec 🐊, soy una IA asistente. ¿Qué deseas hacer? \n1. Información \n2. Comprar \n3. Soporte"
+        );
+        nextStep = "inicio";
+        break;
+
+      case "inicio":
+        const options = steps[step].nextStep;
+        nextStep = options[Number.parseInt(message.text) - 1];
+        await sendWhatsappText(user.phone, `Haz seleccionado ${nextStep}`);
+        break;
+      default:
+        // Para hacer pruebas si sé sale del rango lo voy a reiniciar
+        nextStep = "nuevo";
+        await sendWhatsappText(
+          user.phone,
+          `Hasta aquí llego la AI 🐊, reiniciando!`
+        );
+        break;
+    }
+
+    const updatedUser = await updateUserConversationStep(user.phone, nextStep);
+    console.log(updatedUser);
+
+    return { success: "true" };
+  } catch (error) {
+    console.log(error);
   }
-  const user = await getUserByPhone(message.from);
-
-  if (!user) {
-    console.log("Usuario no registrado intento notificarme");
-    return;
-  }
-
-  let nextStep = "";
-  const step = user.conversation_step;
-  console.log(user)
-  switch (step) {
-    case "nuevo":
-      await sendWhatsappText(
-        user.phone,
-        "Bienvenido a CaimanTec 🐊, soy una IA asistente. ¿Qué deseas hacer? \n1. Información \n2. Comprar \n3. Soporte"
-      );
-      nextStep = "inicio";
-      break;
-
-    case "inicio":
-      const options = steps[step].nextStep;
-      nextStep = options[Number.parseInt(message.text) - 1];
-      await sendWhatsappText(user.phone, `Haz seleccionado ${nextStep}`);
-      break;
-    default:
-      // Para hacer pruebas si sé sale del rango lo voy a reiniciar
-      nextStep = "nuevo";
-      await sendWhatsappText(
-        user.phone,
-        `Hasta aquí llego la AI 🐊, reiniciando!`
-      );
-      break;
-  }
-
-  const updatedUser = await updateUserConversationStep(user.phone, nextStep);
-  console.log(updatedUser);
-
-  return { success: "true" };
 });
 
 server.post("/chatgpt", async (request, reply) => {
