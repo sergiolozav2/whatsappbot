5f0d2b941c4c9ebb3e26199e9eab7b6801e91089
sergiolozav <sergiolozav@gmail.com>
2023-10-26 18:51:58 -0400

    Corregido destructurado de webhook

--- src/services/whatsapp.js
+++ src/services/whatsapp.js
@@ -15,37 +15,32 @@ export async function sendWhatsappText(to, message) {
     type: "text",
     message: message,
     instance_id: instance_id,
-    access_token: account_token
+    access_token: account_token,
   };
   const response = await axios.post(`${url}send`, data, defaultHeaders);
-  return response
+  return response;
 }
 
 export const MessageTypes = {
-  text: "text",
-  image: "image",
-  unknown: "unknown"
-}
+  notify: "notify",
+  unknown: "unknown",
+};
 
 export function objectFromWebhookResponse(body) {
-  const value = body?.entry[0]?.changes[0]?.value
-  const { messages } = value;
-  const message = messages[0]
-  let text = "";
-  let { from, type = MessageTypes.unknown } = message
-  
-  if(type === MessageTypes.text) {
-    text = message.text.body;
-  }
+  const data = body.data.data;
+  const firstMessage = data.messages[0];
+  const type = data.type;
 
-  return { from, type, text}
+  const phone = firstMessage.key.remoteJid.split("@")[0];
+  const text = firstMessage.message.conversation ?? firstMessage.message.extendedTextMessage.text;
 
+  return { from: phone, type, text };
 }
 
 export async function setWebhook(webhook_url) {
-  const endpoint = `set_webhook?webhook_url=${webhook_url}&enable=true&instance_id=${instance_id}&access_token=${account_token}`
+  const endpoint = `set_webhook?webhook_url=${webhook_url}&enable=true&instance_id=${instance_id}&access_token=${account_token}`;
 
-  const response = await axios.get(`${url}${endpoint}`)
-  console.log(response)
+  const response = await axios.get(`${url}${endpoint}`);
+  console.log(response);
   return response;
 }
